b97b331cea65691660eaf3f844da1f1546627977
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-07-26 19:31:46 +1000

    Basic layout
    - SpreadsheetAppBarTop, SpreadsheetFormula, SpreadsheetContent
    - No events wired etc.

---
+++ src/components/SpreadsheetFormula/SpreadsheetFormula.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
+
+export default function SpreadsheetFormula() {
+    return (
+        <TextField id="outlined-basic" variant="outlined" />
+    );
+}
