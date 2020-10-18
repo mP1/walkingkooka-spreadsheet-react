f5cbef05816de5b1e786cb821a1dac465c01338d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-10-18 19:01:22 +1100

    SpreadsheetCellFormat

---
+++ src/spreadsheet/SpreadsheetCellFormat.js
@@ -0,0 +1,33 @@
+export function fromJson(pattern) {
+    return pattern ?
+        new SpreadsheetCellFormat(pattern) :
+        null;
+}
+
+/**
+ * Holds a format pattern within processing of a spreadsheet cell.
+ */
+export default class SpreadsheetCellFormat {
+
+    constructor(pattern) {
+        if (!pattern) {
+            throw new Error("pattern missing");
+        }
+        if (typeof pattern != "string") {
+            throw new Error("Expected string got " + pattern);
+        }
+        this.patternValue = pattern;
+    }
+
+    pattern() {
+        return this.patternValue;
+    }
+
+    toJson() {
+        return this.pattern();
+    }
+
+    toString() {
+        return this.pattern();
+    }
+}
