688f4a7ac7e37501927e72b558cc66cb7db239fd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-11-14 19:05:11 +1100

    Warnings fix
    - 3 minor warnings fixed.

--- src/components/SpreadsheetFormula/SpreadsheetFormula.js
+++ src/components/SpreadsheetFormula/SpreadsheetFormula.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-}));
 
 export default function SpreadsheetFormula() {
     return (
