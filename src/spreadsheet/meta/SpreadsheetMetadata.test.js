0d32aa26ef4b30c852342581e2cce8c749bac885
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-11-14 17:41:20 +1100

    SpreadsheetMetadata.test.js internal naming improvements

--- src/spreadsheet/meta/SpreadsheetMetadata.test.js
+++ src/spreadsheet/meta/SpreadsheetMetadata.test.js
@@ -1,76 +1,76 @@
 import SpreadsheetMetadata from "./SpreadsheetMetadata";
 
 test("Empty", () => {
-    const spreadsheet = SpreadsheetMetadata.EMPTY;
-    expect(spreadsheet).toBeDefined();
+    const metadata = SpreadsheetMetadata.EMPTY;
+    expect(metadata).toBeDefined();
 
-    checkJson(spreadsheet, {});
-    checkSpreadsheetId(spreadsheet);
-    checkSpreadsheetName(spreadsheet);
+    checkJson(metadata, {});
+    checkSpreadsheetId(metadata);
+    checkSpreadsheetName(metadata);
 });
 
 test("from json", () => {
     const json = {};
 
-    const spreadsheet = new SpreadsheetMetadata(json);
+    const metadata = new SpreadsheetMetadata(json);
 
-    checkJson(spreadsheet, json);
-    checkSpreadsheetId(spreadsheet);
-    checkSpreadsheetName(spreadsheet);
+    checkJson(metadata, json);
+    checkSpreadsheetId(metadata);
+    checkSpreadsheetName(metadata);
 });
 
-test("from json with spreadsheet-id", () => {
+test("from json with metadata-id", () => {
     const id = "123f";
-    const json = {"spreadsheet-id": id};
+    const json = {"metadata-id": id};
 
-    const spreadsheet = new SpreadsheetMetadata(json);
+    const metadata = new SpreadsheetMetadata(json);
 
-    checkJson(spreadsheet, json);
-    checkSpreadsheetId(spreadsheet, id);
-    checkSpreadsheetName(spreadsheet);
+    checkJson(metadata, json);
+    checkSpreadsheetId(metadata, id);
+    checkSpreadsheetName(metadata);
 })
 
-test("from json with spreadsheet-name", () => {
+test("from json with metadata-name", () => {
     const name = "Spreadsheet-abc-123";
-    const json = {"spreadsheet-name": name};
-    const spreadsheet = new SpreadsheetMetadata(json);
+    const json = {"metadata-name": name};
+    const metadata = new SpreadsheetMetadata(json);
 
-    checkJson(spreadsheet, json);
-    checkSpreadsheetId(spreadsheet);
-    checkSpreadsheetName(spreadsheet, name);
+    checkJson(metadata, json);
+    checkSpreadsheetId(metadata);
+    checkSpreadsheetName(metadata, name);
 })
 
 test("setSpreadsheetName same", () => {
-    const name = "old-spreadsheet-name-111";
-    const json = {"spreadsheet-name": name};
+    const name = "old-metadata-name-111";
+    const json = {"metadata-name": name};
 
-    const spreadsheet = new SpreadsheetMetadata(json);
-    const same = spreadsheet.setSpreadsheetName(name);
-    expect(spreadsheet).toEqual(same);
+    const metadata = new SpreadsheetMetadata(json);
+    const same = metadata.setSpreadsheetName(name);
+    expect(metadata).toEqual(same);
 
-    checkJson(spreadsheet, json);
-    checkSpreadsheetName(spreadsheet, name);
+    checkJson(metadata, json);
+    checkSpreadsheetName(metadata, name);
 })
 
 test("setSpreadsheetName different name", () => {
     const id = "123f";
-    const name = "old-spreadsheet-name-111";
-    const json = {"spreadsheet-name": name, "spreadsheet-id": id};
+    const name = "old-metadata-name-111";
+    const json = {"metadata-name": name, "metadata-id": id};
 
-    const spreadsheet = new SpreadsheetMetadata(json);
+    const metadata = new SpreadsheetMetadata(json);
 
-    const newName = "new-spreadsheet-name-222";
-    const updated = spreadsheet.setSpreadsheetName(newName);
-    expect(spreadsheet == updated).toBeFalsy();
+    const newName = "new-metadata-name-222";
+    const updated = metadata.setSpreadsheetName(newName);
+    expect(metadata == updated).toBeFalsy();
 
     checkSpreadsheetId(updated, id);
     checkSpreadsheetName(updated, newName);
-    checkJson(updated, {"spreadsheet-name": newName, "spreadsheet-id": id});
+    checkJson(updated, {"metadata-name": newName, "metadata-id": id});
 
     // original should be unmodified
-    checkJson(spreadsheet, json);
-    checkSpreadsheetId(spreadsheet, id);
-    checkSpreadsheetName(spreadsheet, name);
+    checkJson(metadata, json);
+    checkSpreadsheetId(metadata, id);
+    checkSpreadsheetName(metadata, name);
 })
 
 // get..................................................................................................................
@@ -79,21 +79,21 @@ test("property present without defaults", () => {
     const propertyName = "creator";
     const propertyValue = "user1@example.com";
 
-    const spreadsheet = new SpreadsheetMetadata({"creator": propertyValue});
-    expect(spreadsheet.get(propertyName)).toEqual(propertyValue);
+    const metadata = new SpreadsheetMetadata({"creator": propertyValue});
+    expect(metadata.get(propertyName)).toEqual(propertyValue);
 })
 
 test("property missing without defaults", () => {
-    const spreadsheet = new SpreadsheetMetadata({});
-    expect(spreadsheet.get("creator")).toBeUndefined();
+    const metadata = new SpreadsheetMetadata({});
+    expect(metadata.get("creator")).toBeUndefined();
 })
 
 test("property missing but defaulted", () => {
     const propertyName = "creator";
     const propertyValue = "user1@example.com";
 
-    const spreadsheet = new SpreadsheetMetadata({_defaults: {"creator": propertyValue}});
-    expect(spreadsheet.get(propertyName)).toEqual(propertyValue);
+    const metadata = new SpreadsheetMetadata({_defaults: {"creator": propertyValue}});
+    expect(metadata.get(propertyName)).toEqual(propertyValue);
 })
 
 // defaults..............................................................................................................
@@ -102,9 +102,9 @@ test("defaults", () => {
     const propertyValue = "user1@example.com";
     const defaultJson = {"creator": propertyValue};
 
-    const spreadsheet = new SpreadsheetMetadata({_defaults: defaultJson});
-    const defaultSpreadsheetMetadata = spreadsheet.defaults();
-    expect(defaultSpreadsheetMetadata).toEqual(new SpreadsheetMetadata(defaultJson));
+    const metadata = new SpreadsheetMetadata({_defaults: defaultJson});
+    const defaultMetadata = metadata.defaults();
+    expect(defaultMetadata).toEqual(new SpreadsheetMetadata(defaultJson));
 })
 
 test("set defaults missing fails", () => {
@@ -116,21 +116,21 @@ test("set defaults wrong type fails", () => {
 })
 
 test("set defaults EMPTY", () => {
-    const spreadsheet = SpreadsheetMetadata.EMPTY;
-    const same = spreadsheet.setDefaults(SpreadsheetMetadata.EMPTY);
-    expect(same).toEqual(spreadsheet);
+    const metadata = SpreadsheetMetadata.EMPTY;
+    const same = metadata.setDefaults(SpreadsheetMetadata.EMPTY);
+    expect(same).toEqual(metadata);
 })
 
 test("set defaults same", () => {
-    const spreadsheet = new SpreadsheetMetadata({});
-    const same = spreadsheet.setDefaults(SpreadsheetMetadata.EMPTY);
-    expect(same).toEqual(spreadsheet);
+    const metadata = new SpreadsheetMetadata({});
+    const same = metadata.setDefaults(SpreadsheetMetadata.EMPTY);
+    expect(same).toEqual(metadata);
 })
 
 test("set defaults same 2", () => {
-    const defaultSpreadsheetMetadata = new SpreadsheetMetadata({"creator": "user1@example.com"});
-    const withDefaults = new SpreadsheetMetadata({_defaults: defaultSpreadsheetMetadata.toJson()})
-    const same = withDefaults.setDefaults(defaultSpreadsheetMetadata);
+    const defaultMetadata = new SpreadsheetMetadata({"creator": "user1@example.com"});
+    const withDefaults = new SpreadsheetMetadata({_defaults: defaultMetadata.toJson()})
+    const same = withDefaults.setDefaults(defaultMetadata);
     expect(same).toEqual(withDefaults);
 })
 
@@ -151,21 +151,21 @@ test("set defaults different", () => {
 
 // helpers..............................................................................................................
 
-function checkSpreadsheetId(spreadsheet, id) {
-    if(id) {
-        expect(spreadsheet.spreadsheetId()).toBe(id);
+function checkSpreadsheetId(metadata, id) {
+    if (id) {
+        expect(metadata.spreadsheetId()).toBe(id);
     } else {
-        expect(() => spreadsheet.spreadsheetId()).toThrow("Missing \"spreadsheet-id\" " + spreadsheet.toString());
+        expect(() => metadata.spreadsheetId()).toThrow("Missing \"metadata-id\" " + metadata.toString());
     }
 }
 
-function checkSpreadsheetName(spreadsheet, name) {
-    expect(spreadsheet.spreadsheetName()).toBe(name);
+function checkSpreadsheetName(metadata, name) {
+    expect(metadata.metadataName()).toBe(name);
 }
 
 // checks toJson and toString
-function checkJson(spreadsheet, json) {
-    expect(spreadsheet.toObject()).toEqual(json);
-    expect(spreadsheet.toJson()).toStrictEqual(json);
-    expect(spreadsheet.toString()).toBe(JSON.stringify(json));
+function checkJson(metadata, json) {
+    expect(metadata.toObject()).toEqual(json);
+    expect(metadata.toJson()).toStrictEqual(json);
+    expect(metadata.toString()).toBe(JSON.stringify(json));
 }
