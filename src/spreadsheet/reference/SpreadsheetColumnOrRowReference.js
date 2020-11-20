6a6a67b65db31c7edd12d57049ae3d4ab35a5337
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-11-20 19:45:09 +1100

    SpreadsheetColumnOrRowReference.setKind parameter check change

--- src/spreadsheet/reference/SpreadsheetColumnOrRowReference.js
+++ src/spreadsheet/reference/SpreadsheetColumnOrRowReference.js
@@ -28,13 +28,6 @@ export default class SpreadsheetColumnOrRowReference {
      * Helper only intended to be called by sub classes.
      */
     setKind0(kind, factory) {
-        if (!kind) {
-            throw new Error("Missing kind");
-        }
-        if (!(kind instanceof SpreadsheetReferenceKind)) {
-            throw new Error("Expected SpreadsheetReferenceKind kind got " + kind);
-        }
-
         return this.kind() === kind ?
             this :
             new factory(this.value(), kind);
