ab0b6bb3518a8f560430450338b0966ed5ff9341
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-04 20:40:45 +1100

    SpreadsheetNameWidget moved

--- src/widget/SpreadsheetAppBarTop.js
+++ src/widget/SpreadsheetAppBarTop.js
@@ -5,7 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import SpreadsheetNameWidget from './SpreadsheetNameWidget.js';
+import SpreadsheetNameWidget from '../spreadsheet/SpreadsheetNameWidget.js';
 import PropTypes from "prop-types";
 
 /**
