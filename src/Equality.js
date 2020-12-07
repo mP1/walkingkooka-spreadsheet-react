787c6f6c2b249b0e7c30d8ec2e1621981488bc9f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-07 17:18:48 +1100

    Equality.safeEquals Array support

--- src/Equality.js
+++ src/Equality.js
@@ -4,7 +4,29 @@
 export default class Equality {
 
     static safeEquals(left, right) {
-        return left === right ||
-            (left && left.equals && left.equals(right) || left === right);
+        return Array.isArray(left) && equalsArray(left, right) ||
+            typeof left === "object" && equalsObject(left, right) ||
+            left === right;
     }
+}
+
+function equalsArray(left, right) {
+    var equals = Array.isArray(right);
+
+    if (equals) {
+        const length = left.length;
+        var equals = length === right.length;
+
+        if (equals) {
+            for (var i = 0; equals && i < length; i++) {
+                equals = Equality.safeEquals(left[i], right[i]);
+            }
+        }
+    }
+
+    return equals;
+}
+
+function equalsObject(left, right) {
+    return (left && left.equals && left.equals(right) || left === right);
 }
--- src/Equality.test.js
+++ src/Equality.test.js
@@ -54,7 +54,71 @@ test("safeEquals equals implements equals false", () => {
 test("safeEquals equals implements equals true", () => {
     const right = 1;
     const left = {
-        equals: function() { return true},
+        equals: function () {
+            return true
+        },
     }
+    expect(Equality.safeEquals(left, right)).toBeTrue();
+});
+
+// safeEquals array......................................................................................................
+
+test("safeEquals equals null & empty array false", () => {
+    const left = null;
+    const right = [];
+    expect(Equality.safeEquals(left, right)).toBeFalse();
+});
+
+test("safeEquals equals empty array & null false", () => {
+    const left = [];
+    const right = null;
+    expect(Equality.safeEquals(left, right)).toBeFalse();
+});
+
+test("safeEquals equals empty array & empty array true", () => {
+    const left = [];
+    const right = [];
+    expect(Equality.safeEquals(left, right)).toBeTrue();
+});
+
+test("safeEquals equals array different false", () => {
+    const left = [];
+    const right = ["different"];
+    expect(Equality.safeEquals(left, right)).toBeFalse();
+});
+
+test("safeEquals equals array different #2 false", () => {
+    const left = ["same"];
+    const right = ["same", "different"];
+    expect(Equality.safeEquals(left, right)).toBeFalse();
+});
+
+test("safeEquals equals array different #3 false", () => {
+    const left = ["a", "b"];
+    const right = ["b", "a"];
+    expect(Equality.safeEquals(left, right)).toBeFalse();
+});
+
+test("safeEquals equals array same true", () => {
+    const left = ["same"];
+    const right = ["same"];
+    expect(Equality.safeEquals(left, right)).toBeTrue();
+});
+
+test("safeEquals equals array same implement equals true", () => {
+    const rightElement = 1;
+
+    const left = [{
+        equals: function (e) {
+            return e === rightElement
+        },
+    }];
+    const right = [rightElement];
+    expect(Equality.safeEquals(left, right)).toBeTrue();
+});
+
+test("safeEquals equals array deep true", () => {
+    const left = [["same1"], "same2", 3];
+    const right = [["same1"], "same2", 3];
     expect(Equality.safeEquals(left, right)).toBeTrue();
 });
