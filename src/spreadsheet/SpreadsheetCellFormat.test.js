4839d280b238bdd7d31a23071504bb195bec4343
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-07 14:24:15 +1100

    SpreadsheetCellFormat.equals

--- src/spreadsheet/SpreadsheetCellFormat.test.js
+++ src/spreadsheet/SpreadsheetCellFormat.test.js
@@ -1,6 +1,11 @@
 import SpreadsheetCellFormat from "./SpreadsheetCellFormat";
+import SpreadsheetError from "./SpreadsheetError.js";
 
 const pattern = "###-123-abc";
+
+function cellFormat() {
+    return new SpreadsheetCellFormat(pattern);
+}
 
 test("create without pattern fails", () => {
     expect(() => new SpreadsheetCellFormat(null)).toThrow("Missing pattern");
@@ -23,6 +28,44 @@ test("json", () => {
     const spreadsheetCellFormat = new SpreadsheetCellFormat(pattern);
 
     check(spreadsheetCellFormat, pattern);
+});
+
+// equals...............................................................................................................
+
+test("equals undefined false", () => {
+    const f = cellFormat();
+    expect(f.equals()).toBe(false);
+});
+
+test("equals null false", () => {
+    const f = cellFormat();
+    expect(f.equals(null)).toBe(false);
+});
+
+test("equals different type false", () => {
+    const f = cellFormat();
+    expect(f.equals("different")).toBe(false);
+});
+
+test("equals self true", () => {
+    const f = cellFormat();
+    expect(f.equals(f)).toBe(true);
+});
+
+test("equals different false", () => {
+    const f = cellFormat();
+    expect(f.equals(new SpreadsheetError("different"))).toBe(false);
+});
+
+test("equals equivalent true", () => {
+    const f = cellFormat();
+    expect(f.equals(f)).toBe(true);
+});
+
+test("equals equivalent true #2", () => {
+    const format = "#";
+    const f = new SpreadsheetCellFormat(format);
+    expect(f.equals(new SpreadsheetCellFormat(format))).toBe(true);
 });
 
 // helpers..............................................................................................................
