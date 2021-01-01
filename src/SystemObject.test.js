1b25f6ca99e891b6bdad528862e937bfd6893608
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-01 15:54:34 +1100

    SystemObject.fromJsonList & fromJsonListWithType

--- src/SystemObject.test.js
+++ src/SystemObject.test.js
@@ -1,4 +1,5 @@
 import Character from "./Character.js";
+import LocalDateTime from "./datetime/LocalDateTime.js";
 import SystemObject from "./SystemObject.js";
 
 // register.............................................................................................................
@@ -82,6 +83,111 @@ test("fromJsonWithType", () => {
     }))
         .toStrictEqual(character);
 });
+
+// fromJsonList.........................................................................................................
+
+test("fromJsonList missing array undefined fails", () => {
+    expect(
+        () => SystemObject.fromJsonList(undefined, Character.fromJson)
+    ).toThrow("Missing array");
+});
+
+test("fromJsonList missing array null fails", () => {
+    expect(
+        () => SystemObject.fromJsonList(null, Character.fromJson)
+    ).toThrow("Missing array");
+});
+
+test("fromJsonList missing element factory undefined fails", () => {
+    expect(
+        () => SystemObject.fromJsonList([], undefined)
+    ).toThrow("Missing element");
+});
+
+test("fromJsonList missing element factory null fails", () => {
+    expect(
+        () => SystemObject.fromJsonList([], null)
+    ).toThrow("Missing element");
+});
+
+test("fromJsonList empty array", () => {
+    const neverHappens = function(json) {
+        throw new Error("Shouldnt happen");
+    }
+    expect(
+        SystemObject.fromJsonList(
+            [],
+            neverHappens
+        )
+    ).toStrictEqual([]);
+});
+
+test("fromJsonList Character", () => {
+    expect(
+        SystemObject.fromJsonList(
+            [
+                "M",
+                "N",
+                "O"
+            ],
+            Character.fromJson)
+    ).toStrictEqual([
+        Character.fromJson("M"),
+        Character.fromJson("N"),
+        Character.fromJson("O"),
+    ]);
+});
+
+test("fromJsonList LocalDateTime", () => {
+    const localDateTime1 = LocalDateTime.fromJson("1999-1-31 12:58:59");
+    const localDateTime2 = LocalDateTime.fromJson("2000-2-28 12:58:59");
+    const localDateTime3 = LocalDateTime.fromJson("2000-3-28 12:58:59");
+
+    expect(
+        SystemObject.fromJsonList(
+            [
+                localDateTime1.toJson(),
+                localDateTime2.toJson(),
+                localDateTime3.toJson()
+            ],
+            LocalDateTime.fromJson)
+    ).toStrictEqual([
+        localDateTime1,
+        localDateTime2,
+        localDateTime3
+    ]);
+});
+
+// fromJsonListWithType.................................................................................................
+
+test("fromJsonListWithType missing array undefined fails", () => {
+    expect(
+        () => SystemObject.fromJsonListWithType(undefined)
+    ).toThrow("Missing array");
+});
+
+test("fromJsonListWithType missing array null fails", () => {
+    expect(
+        () => SystemObject.fromJsonListWithType(null)
+    ).toThrow("Missing array");
+});
+
+test("fromJsonListWithType", () => {
+    const localDateTime1 = LocalDateTime.fromJson("1999-1-31 12:58:59");
+    const character2 = Character.fromJson("X");
+
+    expect(
+        SystemObject.fromJsonListWithType([
+            localDateTime1.toJsonWithType(),
+            character2.toJsonWithType()
+        ])
+    ).toStrictEqual([
+        localDateTime1,
+        character2
+    ]);
+});
+
+// toJsonWithType.......................................................................................................
 
 test("toJsonWithType", () => {
     const character = new Character("A");
