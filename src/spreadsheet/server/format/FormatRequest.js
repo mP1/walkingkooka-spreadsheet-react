06d2c3f8fe4cba07f37eb921d9aca33bb5ad714c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-01 13:40:26 +1100

    FormatRequest
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-react/issues/410
    - FormatRequest

---
+++ src/spreadsheet/server/format/FormatRequest.js
@@ -0,0 +1,69 @@
+import Equality from "../../../Equality.js";
+import SpreadsheetPattern from "../../format/SpreadsheetPattern.js";
+import SystemObject from "../../../SystemObject.js";
+
+export default class FormatRequest extends SystemObject {
+
+    static fromJson(json) {
+        if(!json){
+            throw new Error("Missing json");
+        }
+        if(typeof json !== "object"){
+            throw new Error("Expected object got " + json);
+        }
+
+        const {value, pattern} = json;
+        if(!value){
+            throw new Error("Missing json.value");
+        }
+        if(!pattern){
+            throw new Error("Missing json.pattern");
+        }
+        return new FormatRequest(
+            SystemObject.fromJsonWithType(value),
+            SystemObject.fromJsonWithType(pattern),
+        );
+    }
+
+    constructor(value, pattern) {
+        super();
+        if(!value && (value !== "" && value !== 0)){
+            throw new Error("Missing value");
+        }
+        if(!pattern){
+            throw new Error("Missing pattern");
+        }
+        if(!(pattern instanceof SpreadsheetPattern)){
+            throw new Error("Expected SpreadsheetPattern pattern got " + pattern);
+        }
+        this.valueValue = value;
+        this.patternValue = pattern;
+    }
+
+    value() {
+        return this.valueValue;
+    }
+
+    pattern() {
+        return this.patternValue;
+    }
+
+    toJson() {
+        return {
+            value: this.value().toJsonWithType(),
+            pattern: this.pattern().toJsonWithType(),
+        };
+    }
+
+    equals(other) {
+        return this === other ||
+            (other instanceof FormatRequest &&
+                Equality.safeEquals(this.value(), other.value()) &&
+                Equality.safeEquals(this.pattern(), other.pattern())
+            );
+    }
+
+    toString() {
+        return this.toJson();
+    }
+}
