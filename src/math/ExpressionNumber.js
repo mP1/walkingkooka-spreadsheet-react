0cc953cc6ed6228987b111de9eacc05da7069267
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-01 20:54:38 +1100

    ExpressionNumber missing text fix

--- src/math/ExpressionNumber.js
+++ src/math/ExpressionNumber.js
@@ -13,7 +13,7 @@ export default class ExpressionNumber extends SystemObject {
 
     constructor(text) {
         super();
-        if(!text && text !== ""){
+        if(null == text){
             throw new Error("Missing text");
         }
         if(text === ""){
