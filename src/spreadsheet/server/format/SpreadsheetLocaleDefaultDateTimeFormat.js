c6ea5dc916676c78d5c26cf7cffdb146da6795fd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-02 12:04:22 +1100

    SpreadsheetLocaleDefaultDateTimeFormat
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-react/issues/414
    - SpreadsheetLocaleDefaultDateTimeFormat

---
+++ src/spreadsheet/server/format/SpreadsheetLocaleDefaultDateTimeFormat.js
@@ -0,0 +1,40 @@
+import SystemObject from "../../../SystemObject.js";
+
+export default class SpreadsheetLocaleDefaultDateTimeFormat extends SystemObject {
+
+    static TYPE_NAME = "spreadsheet-locale-default-date-time-format";
+
+    static INSTANCE = new SpreadsheetLocaleDefaultDateTimeFormat();
+
+    static fromJson(json) {
+        if(!json){
+            throw new Error("Missing json");
+        }
+        if(1 !== json){
+            throw new Error("Expected number 1 got " + json);
+        }
+        return SpreadsheetLocaleDefaultDateTimeFormat.INSTANCE;
+    }
+
+    constructor() {
+        super();
+    }
+
+    equals(other) {
+        return this === other;
+    }
+
+    toJson() {
+        return 1;
+    }
+
+    typeName() {
+        return SpreadsheetLocaleDefaultDateTimeFormat.TYPE_NAME;
+    }
+
+    toString() {
+        return "SpreadsheetLocaleDefaultDateTimeFormat";
+    }
+}
+
+SystemObject.register(SpreadsheetLocaleDefaultDateTimeFormat.TYPE_NAME, SpreadsheetLocaleDefaultDateTimeFormat.fromJson);
