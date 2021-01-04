5de96be8829c1bd4147b1a8140acce43a0791232
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-04 15:05:02 +1100

    SpreadsheetDrawerWidgetTextField extracted from SpreadsheetDrawerWidgetCharacter

--- src/spreadsheet/drawer/SpreadsheetDrawerWidgetCharacter.js
+++ src/spreadsheet/drawer/SpreadsheetDrawerWidgetCharacter.js
@@ -1,78 +1,36 @@
 import Character from "../../Character.js";
 import React from 'react';
-import TextField from "@material-ui/core/TextField";
+import SpreadsheetDrawerWidgetTextField from "./SpreadsheetDrawerWidgetTextField.js";
 import SpreadsheetDrawerWidgetValue from "./SpreadsheetDrawerWidgetValue.js";
 
 /**
  * A widget which displays a {@link Character} for editing using a TextField. All edits immediately update the spreadsheet.
  */
-export default class SpreadsheetDrawerWidgetCharacter extends SpreadsheetDrawerWidgetValue {
+export default class SpreadsheetDrawerWidgetCharacter extends SpreadsheetDrawerWidgetTextField {
 
     constructor(props) {
         super(props);
     }
 
-
-    renderInput(id, value) {
-        return <TextField
-            id={id + "-text"}
-            key={value}
-            style={
-                {
-                    height: "1em",
-                    margin: 0,
-                }
-            }
-            placeholder="Enter 1 character"
-            helperText=""
-            fullWidth
-            margin="normal"
-            disabled={false}
-            InputLabelProps={
-                {
-                    shrink: true,
-                }
-            }
-            defaultValue={value}
-            inputProps={
-                {
-                    size: 1,
-                    maxLength: 1,
-                }
-            }
-            onChange={this.onInputChange.bind(this)}
-            onKeyDown={this.onInputKeyDown.bind(this)}
-        />
+    placeholder() {
+        return "Enter 1 character";
     }
 
-    /**
-     * Receives changes to the input text field.
-     */
-    onInputChange(e) {
-        const string = e.target.value;
-        console.log("onInputChange " + string);
+    maxLength() {
+        return 1;
+    }
+
+    createValue(string) {
+        var value;
 
         switch(string.length) {
             case 0:
-                this.setValue();
-                break;
-            case 1:
-                this.setValue(Character.fromJson(string));
                 break;
             default:
+                value = Character.fromJson(string);
                 break;
         }
-    }
-
-    onInputKeyDown(e) {
-        switch(e.key) {
-            case "Escape":
-                this.onSetDefaultValue();
-                break;
-            default:
-                // nothing special to do for other keys
-                break;
-        }
+        return value;
     }
 }
 
