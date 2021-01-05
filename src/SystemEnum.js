3a41492c0133377cf4851e317f7ebfc8aa2576d9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-05 16:17:08 +1100

    SystemEnum removed unused var
    - NEWUnused variable typeNameToFromJson.

--- src/SystemEnum.js
+++ src/SystemEnum.js
@@ -3,8 +3,6 @@
  */
 import SystemObject from "./SystemObject.js";
 
-const typeNameToFromJson = new Map();
-
 export default class SystemEnum extends SystemObject {
 
     // eslint-disable-next-line no-useless-constructor
