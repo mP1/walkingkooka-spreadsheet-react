5ad33a838554ce53bcd56f4ed45515d3b42306a4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-02-14 11:46:02 +1100

    Settings Slider includes "Default" as first option.
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-react/issues/571
    - Settings slider needs to support "none" option

--- src/spreadsheet/drawer/SpreadsheetDrawerWidgetSlider.js
+++ src/spreadsheet/drawer/SpreadsheetDrawerWidgetSlider.js
@@ -1,3 +1,4 @@
+import Equality from "../../Equality.js";
 import React from 'react';
 import Slider from "@material-ui/core/Slider";
 import SpreadsheetDrawerWidgetValue from "./SpreadsheetDrawerWidgetValue.js";
@@ -5,7 +6,9 @@ import PropTypes from "prop-types";
 import SystemEnum from "../../SystemEnum.js";
 
 /**
- * Shows the enum values of RoundingMode as a slider.
+ * Shows the enum values of an SystemEnum as a slider.
+ * Note the marks and values for the slider will have the first slot reserved for the "Default" value. Clicking
+ * default will result in setValue being called with null.
  */
 export default class SpreadsheetDrawerWidgetSlider extends SpreadsheetDrawerWidgetValue {
 
@@ -15,49 +18,52 @@ export default class SpreadsheetDrawerWidgetSlider extends SpreadsheetDrawerWidg
         const values = props.values;
         const marks = [];
 
-        values.forEach((v, i) =>  {
+        marks.push({
+            label: "Default",
+            value: 0,
+        });
+
+        props.values.forEach((v, i) => {
             marks.push({
                 label: v.label(),
-                value: i,
+                value: i + 1,
             });
         });
         this.values = values;
         this.marks = marks;
-
-        // convert defaultValue to an index.
-        const defaultValue = props.defaultValue;
-        this.defaultValueIndex =  null != defaultValue ?
-            values.indexOf(defaultValue) :
-            defaultValue;
         this.style = props.style;
     }
 
     renderInput(id, value) {
         const {marks} = this;
         const sliderId = id + "-Slider";
+        const sliderValue = this.props.values.findIndex((e) => Equality.safeEquals(e, value)) + 1;
 
         return <Slider id={sliderId}
                        key={sliderId}
-                       defaultValue={this.defaultValueIndex}
+                       defaultValue={0}
                        min={0}
                        max={marks.length - 1}
                        marks={marks}
-                       value={this.value}
+                       value={sliderValue}
                        onChange={this.onChange.bind(this)}
                        style={this.style}
         />;
     }
 
     onChange(e, newValue) {
-        this.setValue(this.values[newValue]);
+        this.setValue(
+            newValue > 0 ?
+                this.values[newValue - 1] :
+                null
+        );
     }
 
     /**
      * Handles the setDefault button being clicked, clearing the value, which lets the default be used and also clears the TextField.
      */
     onSetDefaultValue() {
-        console.log("onSetDefaultValue");
-        this.setValue(this.defaultValue);
+        this.setValue(null);
     }
 }
 
