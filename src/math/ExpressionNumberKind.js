07b500ac297f54cc59a308010d844295fe132a9b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-03-17 21:33:01 +1100

    ExpressionNumberKind generated

--- src/math/ExpressionNumberKind.js
+++ src/math/ExpressionNumberKind.js
@@ -1,36 +1,37 @@
-import ExpressionNumber from "./ExpressionNumber.js";
 import SystemEnum from "../SystemEnum.js";
 import SystemObject from "../SystemObject.js";
+import ExpressionNumber from "./ExpressionNumber.js";
 
 const TYPE_NAME = "expression-number-kind";
 
 export default class ExpressionNumberKind extends SystemEnum {
 
-    static BIG_DECIMAL = new ExpressionNumberKind("BIG_DECIMAL");
-    static DOUBLE = new ExpressionNumberKind("DOUBLE");
-
-    static valueOf(name) {
-        return SystemEnum.valueOf(name, ExpressionNumberKind.values());
-    }
-
-    static values() {
-        return [
-            ExpressionNumberKind.BIG_DECIMAL,
-            ExpressionNumberKind.DOUBLE,
-        ];
-    }
-
-    static fromJson(name) {
-        return ExpressionNumberKind.valueOf(name);
-    }
-
-    constructor(name) {
-        super(name);
-    }
-
-    typeName() {
-        return TYPE_NAME;
-    }
+  static BIG_DECIMAL = new ExpressionNumberKind("BIG_DECIMAL");
+  static DOUBLE = new ExpressionNumberKind("DOUBLE");
+  
+  static values() {
+    return [
+      ExpressionNumberKind.BIG_DECIMAL,
+      ExpressionNumberKind.DOUBLE
+    ];
+  }
+  
+  static valueOf(name) {
+    return SystemEnum.valueOf(name, ExpressionNumberKind.values());
+  }
+  
+  static fromJson(name) {
+    return ExpressionNumberKind.valueOf(name);
+  }
+  
+  constructor(name) {
+    super(name);
+  }
+  
+  typeName() {
+    return TYPE_NAME;
+  }
+  
 }
 
 SystemObject.register(TYPE_NAME, ExpressionNumberKind.fromJson);
