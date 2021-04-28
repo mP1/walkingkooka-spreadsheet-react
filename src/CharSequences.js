114108cdc1d472e091a2b2682c3d884d362173ce
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-04-28 19:14:01 +1000

    CharSequences.escape

---
+++ src/CharSequences.js
@@ -0,0 +1,55 @@
+/**
+ * A variety of general purpose text functions
+ */
+
+function escapeText(text) {
+    var s = "";
+
+    const length = text.length;
+    for(var i = 0; i < length; i++) {
+        const c = text.charAt(i);
+
+        let toString = "";
+        switch(c) {
+            case '\0':
+                toString = "\\0";
+                break;
+            case '\n':
+                toString = "\\n";
+                break;
+            case '\r':
+                toString = "\\r";
+                break;
+            case '\t':
+                toString = "\\t";
+                break;
+            case '\'':
+                toString = "\\'";
+                break;
+            case '"':
+                toString = "\\\"";
+                break;
+            default:
+                const code = c.charCodeAt(0);
+                toString = code < 32 ?
+                    ("\\u0000" + code).substring(0, 6) :
+                    c;
+                break;
+        }
+
+        s = s + toString;
+    }
+    return s;
+}
+
+export default class CharSequences {
+
+    /**
+     * Escapes the given text
+     */
+    static escape(text) {
+        return null == text ?
+            "null" :
+            escapeText(text.toString());
+    }
+}
