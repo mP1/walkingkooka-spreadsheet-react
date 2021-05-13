c379e806157629990feb89cbfc5d62cf76a574b7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-05-13 18:42:10 +1000

    SpreadsheetApp extends SpreadsheetHistoryAwareStateWidget
    - organised methods.
    - renamed methods noun first so naming groups.
    - history listening methods no longer perform actions only update state.
    - improved updating of state within state change methods.
    - spreadsheet/SpreadsheetApp.js replaces App.js
    
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-react/issues/873
    - App extends SpreadsheetHistoryAwareStateWidget

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from "react-router-dom";
 import './index.css';
-import App from './App';
+import SpreadsheetApp from './spreadsheet/SpreadsheetApp.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
         <HashRouter>
-            <App/>
+            <SpreadsheetApp/>
         </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
