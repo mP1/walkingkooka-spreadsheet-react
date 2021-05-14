6f67c6d1a3977c83bc6edd11075da8c6ffdee200
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-05-15 00:28:05 +1000

    SpreadsheetNotificationWidget also logs

--- src/spreadsheet/notification/SpreadsheetNotificationWidget.js
+++ src/spreadsheet/notification/SpreadsheetNotificationWidget.js
@@ -1,3 +1,4 @@
+import Equality from "../../Equality.js";
 import React from 'react';
 
 export default class SpreadsheetNotificationWidget extends React.Component {
@@ -8,8 +9,34 @@ export default class SpreadsheetNotificationWidget extends React.Component {
         this.state = {};
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return !Equality.safeEquals(this.state.notification, nextState.notification);
+    }
+
     componentDidUpdate(prevProps, prevState, snapshot) {
         console.log("componentDidUpdate state.notification: ", "new", this.state.notification, "prevState", prevState.notification);
+
+        const notification = this.state.notification;
+        if(null != notification && !Equality.safeEquals(notification, prevState.notification)){
+            const level = notification.level();
+            const message = notification.text();
+
+            switch(level) {
+                case "error":
+                    console.error(message);
+                    break;
+                case "info":
+                case "success":
+                    console.info(message);
+                    break;
+                case "warning":
+                    console.warn(message);
+                    break;
+                default:
+                    console.error("Unknown level: " + level + ": " + message);
+                    break;
+            }
+        }
     }
 
     render() {
