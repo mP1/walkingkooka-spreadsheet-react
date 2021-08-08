d17d66811cbb8558e5320cffcf2688d0b8ea2c20
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-08-08 19:23:39 +1000

    history hash token cell range support
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-react/issues/1124
    - History hash support for cell range

--- src/spreadsheet/reference/SpreadsheetCellReferenceOrLabelNameParse.js
+++ src/spreadsheet/reference/SpreadsheetCellReferenceOrLabelNameParse.js
@@ -1,11 +1,14 @@
 import Preconditions from "../../Preconditions.js";
+import SpreadsheetCellRange from "./SpreadsheetCellRange.js";
 import SpreadsheetCellReference from "./SpreadsheetCellReference.js";
 import SpreadsheetLabelName from "./SpreadsheetLabelName.js";
 
 export default function spreadsheetCellReferenceOrLabelNameParse(text) {
     Preconditions.requireNonEmptyText(text, "text");
 
-    return SpreadsheetCellReference.isCellReferenceText(text) ?
-        SpreadsheetCellReference.fromJson(text) :
-        SpreadsheetLabelName.fromJson(text);
+    return text.indexOf(":") > 0 ?
+        SpreadsheetCellRange.parse(text) :
+        SpreadsheetCellReference.isCellReferenceText(text) ?
+            SpreadsheetCellReference.fromJson(text) :
+            SpreadsheetLabelName.fromJson(text);
 }
