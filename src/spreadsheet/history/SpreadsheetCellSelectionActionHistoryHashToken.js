8bf16605ac5775e8e42cd89eccea837bab753b3a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-09-18 18:17:47 +1000

    SpreadsheetCellSelectionActionHistoryHashToken

---
+++ src/spreadsheet/history/SpreadsheetCellSelectionActionHistoryHashToken.js
@@ -0,0 +1,7 @@
+import SpreadsheetSelectionActionHistoryHashToken from "./SpreadsheetSelectionActionHistoryHashToken.js";
+
+/**
+ * Base for all cell actions
+ */
+export default class SpreadsheetCellSelectionActionHistoryHashToken extends SpreadsheetSelectionActionHistoryHashToken {
+}
