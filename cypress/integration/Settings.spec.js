3829755eab6f0b06088076dc137152553274e3e4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-09-26 17:02:21 +1000

    Settings integration tests: formula text entry improvements

--- cypress/integration/Settings.spec.js
+++ cypress/integration/Settings.spec.js
@@ -28,6 +28,22 @@ describe(
 
         const testing = new SpreadsheetTesting(cy);
 
+        // helper that includes some waits to make formula text entry more reliable.
+        function formulaTextEntry(a1Formula) {
+            testing.cellClick(A1);
+
+            testing.wait(100); // wait for formula text to load.
+
+            testing.formulaTextClick();
+
+            testing.wait(200);
+
+            testing.formulaText()
+                .type("{selectall}" + a1Formula + "{enter}", FORCE_TRUE);
+
+            testing.wait(200); // wait for save to complete.
+        }
+
         beforeEach(() => {
             cy.visit('/');
 
@@ -189,13 +205,7 @@ describe(
                 }
 
                 if(a1Formula){
-                    testing.cellClick(A1);
-
-                    testing.formulaTextClick();
-
-                    testing.formulaText()
-                        .wait(200)
-                        .type("{selectall}" + a1Formula + "{enter}", FORCE_TRUE);
+                    formulaTextEntry(a1Formula);
                 }
 
                 const textFieldId = "#settings-spreadsheet-metadata-" + property + "-TextField";
@@ -282,12 +292,7 @@ describe(
                 settingsOpenSectionSpreadsheetMetadataProperty(property);
 
                 if(a1Formula){
-                    testing.cellClick(A1);
-
-                    testing.formulaTextClick();
-
-                    testing.formulaText()
-                        .type("{selectall}" + a1Formula + "{enter}", FORCE_TRUE);
+                    formulaTextEntry(a1Formula);
                 }
 
                 const sliderId = "#settings-spreadsheet-metadata-" + property + "-Slider";
@@ -319,12 +324,7 @@ describe(
                 settingsOpenSectionSpreadsheetMetadataProperty(property);
 
                 if(a1Formula){
-                    testing.cellClick(A1);
-
-                    testing.formulaTextClick();
-
-                    testing.formulaText()
-                        .type("{selectall}" + a1Formula + "{enter}", FORCE_TRUE);
+                    formulaTextEntry(a1Formula);
                 }
 
                 const sliderId = "#settings-spreadsheet-metadata-" + property + "-Slider";
@@ -377,12 +377,7 @@ describe(
                 settingsOpenSectionSpreadsheetMetadataProperty(property);
 
                 if(a1Formula){
-                    testing.cellClick(A1);
-
-                    testing.formulaTextClick();
-
-                    testing.formulaText()
-                        .type("{selectall}" + a1Formula + "{enter}", FORCE_TRUE);
+                    formulaTextEntry(a1Formula);
                 }
 
                 const dropDownListId = "#settings-spreadsheet-metadata-" + property + "-DropDownList";
@@ -746,12 +741,7 @@ describe(
 
                 settingsOpenSectionSpreadsheetMetadataStyleProperty(property);
 
-                testing.cellClick(A1);
-
-                testing.formulaTextClick();
-
-                testing.formulaText()
-                    .type("{selectall}'ABC" + "{enter}", FORCE_TRUE);
+                formulaTextEntry("'ABC");
 
                 const textFieldId = "#settings-spreadsheet-metadata-style-" + property + "-TextField";
 
@@ -794,12 +784,7 @@ describe(
 
                 settingsOpenSectionSpreadsheetMetadataStyleProperty(property);
 
-                testing.cellClick(A1);
-
-                testing.formulaTextClick();
-
-                testing.formulaText()
-                    .type("{selectall}'ABC{enter}", FORCE_TRUE);
+                formulaTextEntry("'ABC");
 
                 const sliderId = "#settings-spreadsheet-metadata-style-" + property + "-Slider";
 
@@ -846,12 +831,7 @@ describe(
 
                 settingsOpenSectionSpreadsheetMetadataStyleProperty(property);
 
-                testing.cellClick(A1);
-
-                testing.formulaTextClick();
-
-                testing.formulaText()
-                    .type("{selectall}'ABC{enter}", FORCE_TRUE);
+                formulaTextEntry("'ABC");
 
                 const sliderId = "#settings-spreadsheet-metadata-style-" + property + "-Slider";
                 const numberTextFieldId = "#settings-spreadsheet-metadata-style-" + property + "-NumberTextField";
