952faedc0cccd7ec3f113e29a2863ff83e02f1c3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-10-04 19:04:22 +1100

    SpreadsheetSettingsLoadAndEditHistoryHashToken

--- src/spreadsheet/history/SpreadsheetSettingsHistoryHashToken.js
+++ src/spreadsheet/history/SpreadsheetSettingsHistoryHashToken.js
@@ -1,7 +1,5 @@
-import SpreadsheetHistoryHashToken from "./SpreadsheetHistoryHashToken.js";
-
 /**
  * Base class for any settings history hash token.
  */
-export default class SpreadsheetSettingsHistoryHashToken extends SpreadsheetHistoryHashToken {
+export default class SpreadsheetSettingsHistoryHashToken extends SpreadsheetSettingsHistoryHashToken {
 }
