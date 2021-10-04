adad8d46ec93bb82404d24458c635f4476846ebd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-10-04 18:58:51 +1100

    SpreadsheetSettingsHistoryHashToken

---
+++ src/spreadsheet/history/SpreadsheetSettingsHistoryHashToken.js
@@ -0,0 +1,7 @@
+import SpreadsheetHistoryHashToken from "./SpreadsheetHistoryHashToken.js";
+
+/**
+ * Base class for any settings history hash token.
+ */
+export default class SpreadsheetSettingsHistoryHashToken extends SpreadsheetHistoryHashToken {
+}
