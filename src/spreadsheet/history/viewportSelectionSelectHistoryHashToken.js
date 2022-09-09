d9c1bb676888fbceae956b0e578e6ebf16ee20d7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-09-09 21:28:25 +1000

    viewportSelectionSelectHistoryHashToken was selectHistoryHashToken

--- src/spreadsheet/history/viewportSelectionSelectHistoryHashToken.js
+++ src/spreadsheet/history/viewportSelectionSelectHistoryHashToken.js
@@ -6,7 +6,7 @@ import SpreadsheetColumnOrRowSelectHistoryHashToken
  * Null safe history hash token factory that creates the appropriate {@link SpreadsheetSelectHistoryHashToken} after testing the
  * given {@link SpreadsheetViewportSelection}.
  */
-export default function selectHistoryHashToken(viewportSelection) {
+export default function viewportSelectionSelectHistoryHashToken(viewportSelection) {
     return viewportSelection && viewportSelection.selection().isCellScalarOrRange() ?
         new SpreadsheetCellSelectHistoryHashToken(viewportSelection) :
         new SpreadsheetColumnOrRowSelectHistoryHashToken(viewportSelection);
