7dfef8063f9e9ff2329242bf62cd9712e5a4cfbc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-09-25 13:29:12 +1000

    SpreadsheetCellStyleHistoryHashToken.propertyName extracted

--- src/spreadsheet/reference/cell/style/SpreadsheetCellStyleEditHistoryHashToken.js
+++ src/spreadsheet/reference/cell/style/SpreadsheetCellStyleEditHistoryHashToken.js
@@ -1,31 +1,14 @@
-import Equality from "../../../../Equality.js";
 import SpreadsheetCellStyleHistoryHashToken from "./SpreadsheetCellStyleHistoryHashToken.js";
 import SpreadsheetHistoryHashTokens from "../../../history/SpreadsheetHistoryHashTokens.js";
-import TextStyle from "../../../../text/TextStyle.js";
 
 /**
  * A command that represents a cell style property value save.
  */
 export default class SpreadsheetCellStyleEditHistoryHashToken extends SpreadsheetCellStyleHistoryHashToken {
 
-    constructor(selection, propertyName) {
-        super(selection);
-
-        this.propertyNameValue = TextStyle.checkPropertyName(propertyName);
-    }
-
-    /**
-     * The value which may be null if the property was cleared or removed.
-     */
-    propertyName() {
-        return this.propertyNameValue;
-    }
-
     // /cell/A1/style/font-style/edit
     historyHashPath() {
         return super.historyHashPath() +
-            "/" +
-            this.propertyName() +
             "/" +
             SpreadsheetHistoryHashTokens.EDIT;
     }
@@ -40,10 +23,6 @@ export default class SpreadsheetCellStyleEditHistoryHashToken extends Spreadshee
     }
 
     equals(other) {
-        return this === other ||
-            (
-                other instanceof SpreadsheetCellStyleEditHistoryHashToken &&
-                Equality.safeEquals(this.propertyName(), other.propertyName())
-            );
+        return super.equals(other) && other instanceof SpreadsheetCellStyleEditHistoryHashToken;
     }
 }
